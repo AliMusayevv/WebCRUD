c634923823325c2f578beab3188d5080dc990d77
AliMusayevv <emusayev001@gmail.com>
2022-12-11 04:25:57 -0800

    Add project files.

---
+++ WebCRUD/Connection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebCRUD
+{
+    public class Connection
+    {
+        public static string DS
+        {
+            get
+            {
+                return "Data Source=MARCELINE;Initial Catalog=LoginPage;Integrated Security = True";
+            }
+
+
+        }
+        public static int Update(string test1, string test2, string test3, string test4, string test5,int id)
+
+        {
+            using (SqlConnection conn = new SqlConnection(Connection.DS))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("update WebCRUD set FIRSTNAME=@FIRSTNAME,LASTNAME=@LASTNAME,FATHERNAME=@FATHERNAME,EMAIL=@EMAIL,PHONE=@PHONE where ID=@ID ", conn);
+
+                cmd.Parameters.AddWithValue("@FIRSTNAME", test1);
+                cmd.Parameters.AddWithValue("@LASTNAME", test2);
+                cmd.Parameters.AddWithValue("@FATHERNAME", test3);
+                cmd.Parameters.AddWithValue("@EMAIL", test4);
+                cmd.Parameters.AddWithValue("@PHONE", test5);
+                cmd.Parameters.AddWithValue("@ID", id);
+
+
+
+                int n = cmd.ExecuteNonQuery();
+
+                conn.Close();
+                return n;
+            }
+        }
+        public static int Delete(int id)
+
+        {
+            using (SqlConnection conn = new SqlConnection(Connection.DS))
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("delete from WebCRUD where ID=@ID", conn);
+                cmd.Parameters.AddWithValue("@ID", id);
+                int x = cmd.ExecuteNonQuery();
+
+                conn.Close();
+                return x;
+            }
+
+        }
+    }
+}
---
+++ WebCRUD/index.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebCRUD
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Fill();
+            }
+
+        }
+        protected void Fill()
+        {
+            using (SqlConnection conn = new SqlConnection(Connection.DS)) 
+
+  
+            {
+                conn.Open();
+                SqlCommand cmd;
+                cmd = new SqlCommand("select * from WebCRUD", conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+                if (DR.HasRows == true)
+                {
+                    GrdResults.DataSource = DR;
+                    GrdResults.DataBind();
+                }
+            }
+        }
+
+        protected void GrdResults_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GrdResults.EditIndex = e.NewEditIndex;
+            Fill();
+        }
+
+        protected void GrdResults_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+            string test1 = ((TextBox)GrdResults.Rows[e.RowIndex].Cells[1].Controls[0]).Text;
+            string test2 = ((TextBox)GrdResults.Rows[e.RowIndex].Cells[2].Controls[0]).Text;
+            string test3 = ((TextBox)GrdResults.Rows[e.RowIndex].Cells[3].Controls[0]).Text;
+            string test4 = ((TextBox)GrdResults.Rows[e.RowIndex].Cells[4].Controls[0]).Text;
+            string test5 = ((TextBox)GrdResults.Rows[e.RowIndex].Cells[5].Controls[0]).Text;
+            int id = Convert.ToInt32(GrdResults.DataKeys[e.RowIndex].Value.ToString());
+            using (SqlConnection conn = new SqlConnection(Connection.DS))
+            {
+                int n = Connection.Update(test1,test2,test3 ,test4 ,test5,id );    
+
+                
+                if(n>0)
+                {
+                    Response.Write("<script>alert('Data successfully uptating')</script>");
+                    GrdResults.EditIndex = -1;
+                    Fill();
+                }
+            }
+  
+
+        }
+
+        protected void GrdResults_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        
+        {
+            int id = Convert.ToInt32(GrdResults.DataKeys[e.RowIndex].Value.ToString());
+            SqlConnection conn = new SqlConnection(Connection.DS);
+            int x = Connection.Delete(id);
+            if (x > 0)
+            {
+
+                Response.Write("<script>alert('Data successfully deleting')</script>");
+                GrdResults.EditIndex = -1;
+                Fill();
+            }
+
+
+
+
+
+        }
+
+    protected void GrdResults_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GrdResults.EditIndex = -1;
+            Fill();
+        }
+    }
+}
